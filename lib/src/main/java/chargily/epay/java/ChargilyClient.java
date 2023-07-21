6a8d77ded1febd4c71fad15b6679a3d98e139c81
Sofiane Louchene <mrsofianelouchene@gmail.com>
2023-07-21 21:31:14 +0200

    feat: add async create invoice

--- lib/src/main/java/chargily/epay/java/ChargilyClient.java
+++ lib/src/main/java/chargily/epay/java/ChargilyClient.java
@@ -1,6 +1,7 @@
 package chargily.epay.java;
 
 import br.com.fluentvalidator.exception.ValidationException;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,12 +21,35 @@ public class ChargilyClient {
     private final String apiKey;
     private final ChargilyApi chargilyApi;
 
+    /**
+     * Create invoice synchronously
+     *
+     * @param invoice Invoice information to be sent to Chargily API.
+     * @return ChargilyResponse that contains checkoutUrl.
+     * @throws IOException         if a problem occurred talking to the server.
+     * @throws ValidationException if the invoice object is not valid.
+     * @see #createInvoiceAsync(Invoice, Callback) createInvoiceAsync
+     */
     public Response<ChargilyResponse> createInvoice(Invoice invoice) throws IOException, ValidationException {
         var validations = new InvoiceValidator().validate(invoice);
-        if(!validations.isValid())
+        if (!validations.isValid())
             throw new InvoiceException(validations);
         return chargilyApi.createInvoice(apiKey, invoice).execute();
     }
 
+    /**
+     * Create invoice asynchronously
+     *
+     * @param invoice  Invoice information to be sent to Chargily API.
+     * @param callback Retrofit Callback object with OnResponse and OnFailure.
+     * @throws ValidationException if the invoice object is not valid.
+     * @see #createInvoice(Invoice) createInvoice
+     */
+    public void createInvoiceAsync(Invoice invoice, Callback<ChargilyResponse> callback) throws ValidationException {
+        var validations = new InvoiceValidator().validate(invoice);
+        if (!validations.isValid())
+            throw new InvoiceException(validations);
+        chargilyApi.createInvoice(apiKey, invoice).enqueue(callback);
+    }
 
 }
